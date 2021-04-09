a435ef9c0e4f516efe32e55f273df10d2c09cd58
Marek  Bielan <mareckyb@gmail.com>
2021-04-09 19:24:42 +0200

    Initial Commit

---
+++ src/main/java/com/example/dyplom/DyplomApplication.java
@@ -0,0 +1,13 @@
+package com.example.dyplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyplomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DyplomApplication.class, args);
+    }
+
+}
