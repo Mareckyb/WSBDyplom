74d7f3d01662864a2c3f8f25c2a95c22ff1c41aa
Mareckyb <73360739+Mareckyb@users.noreply.github.com>
2021-05-22 20:38:26 +0200

    Zrobione: USERS TODO: BUG - pole hasło na istniejącym użytkowniku powinno być niewalidowane - podczas edycji już założonego usera - będzie na zajęciach ZROBIONE: Lista Projektów i Lista ISSUES ZROBIONE: Dodanie i walidacja nowego projektu ZROBIONE: edycja Projektu - poprawiona autoryzacja wychwycony błąd i dodane permisssions ZROBIONE: usuwanie Projektu z usuwaniem podłączonych do projektu zadań
    IN PROGRESS: Dodanie nowego, edycja i usuwanie Issue
    
    TODO: Filitrowanie Issues - zajęcia Kasi
    TODO: Wysyłanie maila - zajęcia Kasi
    TODO: Edycja dla zalogowanego usera - login, username pass - bez uprawnień
    TODO: Pacykowanie formularzy

--- src/main/java/com/example/dyplom/issues/IssueRepository.java
+++ src/main/java/com/example/dyplom/issues/IssueRepository.java
@@ -3,7 +3,12 @@ package com.example.dyplom.issues;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    Issue findIssueByProjectId(Long id);
+    List<Issue> findAllByProjectId(Long id);
+
 
 }
--- src/main/java/com/example/dyplom/projects/ProjectController.java
+++ src/main/java/com/example/dyplom/projects/ProjectController.java
@@ -1,5 +1,7 @@
 package com.example.dyplom.projects;
 
+import com.example.dyplom.issues.Issue;
+import com.example.dyplom.issues.IssueRepository;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -7,14 +9,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
 
     final ProjectRepository projectRepository;
+    final IssueRepository issueRepository;
 
-    public ProjectController(ProjectRepository projectRepository) { this.projectRepository = projectRepository;}
+    public ProjectController(ProjectRepository projectRepository, IssueRepository issueRepository) { this.projectRepository = projectRepository;
+        this.issueRepository = issueRepository;
+    }
 
     @GetMapping("/")
     ModelAndView index() {
@@ -68,9 +74,11 @@ public class ProjectController {
     ModelAndView deleteProject (@PathVariable Long id) {
         ModelAndView modelAndView = new ModelAndView();
         Project project = projectRepository.findById(id).orElse(null);
+        List<Issue> issueToDelete = issueRepository.findAllByProjectId(id);
         if (project == null) {
             return index();
         }
+        issueRepository.deleteAll(issueToDelete);
         projectRepository.delete(project);
         modelAndView.setViewName("redirect:/project/");
 
